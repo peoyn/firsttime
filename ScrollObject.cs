d677dcaa6e841dbad62eb87e401d5432694bc063
peoyn <95073731+peoyn@users.noreply.github.com>
2023-02-28 00:35:00 +0900

    Add files via upload

---
+++ ScrollObject.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScrollObject : MonoBehaviour
+{
+	public float speed = 1.0f;
+	public float startPosition;
+	public float endPosition;
+	
+	void Update ()
+	{
+		transform.Translate(-1 * speed * Time.deltaTime, 0, 0);
+		
+		if (transform.position.x <= endPosition) ScrollEnd();
+	}
+	
+	void ScrollEnd ()
+	{ 
+		transform.Translate(-1 * (endPosition - startPosition), 0, 0);
+		
+		SendMessage("OnScrollEnd", SendMessageOptions.DontRequireReceiver);
+	}
+}
+
+//MonoBehaviour
+
+//Awake
+//OnEnable
+//Start
+//OnApplicationPause
+//FixedUpdate(1/0.02)
+//Update
+//LateUpdate
+//OnDisable
+//......
+
+// 접근제한(한정)자    
+
+// public
+
+// private
+
+// internal
+
+// protected
---
+++ SealController_1.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SealController_1 : MonoBehaviour
+{
+    Rigidbody2D rb2d;
+
+    public float maxHeight;
+    public float flapVelocity;
+
+    void Awake()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1") && transform.position.y < maxHeight)
+        {
+            Flap();
+        }
+
+    }
+
+    public void Flap()
+    {
+
+        rb2d.velocity = new Vector2(0.0f, flapVelocity);
+    }
+}
